35f38e775bb5a62891ffff7e69f4b706dc7a8997
Dave Knoester <dave.knoester@gmail.com>
2014-07-30 20:51:17 -0400

    added resource slice config option

--- src/logic9.cpp
+++ src/logic9.cpp
@@ -17,7 +17,6 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
 #include <ea/cmdline_interface.h>
 #include <avida4/logic9.h>
 using namespace ealib;
